8bc6cd59c121a37c93e4001d513fd47e9036e71f
jules <jules.cayot@ik.me>
2024-11-17 13:50:23 +0200

    Add merge-insert sort and main function implementation
    Implemented the merge-insert sort algorithm for both lists and vectors in `PmergeMe` class, including appropriate helper functions. Added a main function in `main.cpp` to demonstrate the sorting algorithm with command-line input and measure execution time for both data structures.

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -5,8 +5,30 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
-class PmergeMe {
+#include <list>
+#include <vector>
+#include <sys/types.h>
 
+class PmergeMe {
+	private:
+		static constexpr int ARBITRARY_VALUE = 10;
+
+	public:
+		static std::list<u_int>		mergeInsertSort(const std::list<u_int>& list);
+		static std::vector<u_int>	mergeInsertSort(const std::vector<u_int>& vector);
+
+		~PmergeMe();
+
+	private:
+		static std::list<u_int>		insertSort(const std::list<u_int>& list);
+		static std::vector<u_int>	insertSort(const std::vector<u_int>& vector);
+		static std::list<u_int>		mergeSort(const std::list<u_int>& v1, const std::list<u_int>& v2);
+		static std::vector<u_int>	mergeSort(const std::vector<u_int>& v1, const std::vector<u_int>& v2);
+
+		PmergeMe();
+		PmergeMe(const PmergeMe &);
+
+		PmergeMe &operator=(const PmergeMe &);
 };
 
 #endif
